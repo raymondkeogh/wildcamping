d45578e9cbdfec418df0d98bee7c20f54eb8bb9e
raymondkeogh <raymondkeogh@gmail.com>
2021-04-28 20:17:09 +0000

    Connect google maps API

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge:"right"});
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        edge: "right"
+    });
 });
+
+
+let map;
+// Google maps api
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8,
+    });
+}
