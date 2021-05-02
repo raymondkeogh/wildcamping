e1688852ca4dce83dfc0f7664dbf6bdfc2299014
raymondkeogh <raymondkeogh@gmail.com>
2021-05-02 21:16:53 +0000

    Add login authentication functionality

--- app.py
+++ app.py
@@ -55,6 +55,26 @@ def signup():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # Check if username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            # Check user password
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")): 
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            
+            else:
+                # incorrect passord
+                flash("Username or Password incorrect, Please try again")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Username or Password incorrect, Please try again")
+            return redirect(url_for("login"))
     return render_template("login.html")
 
 
