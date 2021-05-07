8bf11e9abd86de1bf36f5ccee9f02f10352ed292
raymondkeogh <raymondkeogh@gmail.com>
2021-05-07 22:07:03 +0000

    Change map code to allow for JSON data input

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
     Flask, flash, render_template,
-    redirect, request, session, url_for)
+    redirect, request, session, url_for, jsonify)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -59,14 +59,14 @@ def login():
         # Check if username already exists
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        
+
         if existing_user:
             # Check user password
             if check_password_hash(
-                existing_user["password"], request.form.get("password")): 
+                    existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
-            
+                return render_template("profile.html")
             else:
                 # incorrect passord
                 flash("Username or Password incorrect, Please try again")
@@ -83,13 +83,16 @@ def profile_page(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-
+    # locations = mongo.db.locations.find(
+    #     {"username": session["user"]})
     if session["user"]:
-        return render_template("profile.html", username=username)
+        return render_template(
+            "profile.html", username=username)
     else:
         return redirect(url_for("login"))
 
 
+@app.route("/profile_page/<username>/user_locations")
 @app.route("/logout")
 def logout():
     flash("You have been logged out")
@@ -97,26 +100,38 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_location", methods=["GET", "POST"])
-def add_location():
+@app.route("/user_location", methods=["GET", "POST"])
+def user_location():
     if request.method == "POST":
-        lat = request.form.get("lat")
-        lng = request.form.get("lng")
-        latlng = lat + ", " + lng
         new_location = {
             "name": request.form.get("location_name"),
             "description": request.form.get("location_description"),
             "rating": request.form.get("rating"),
-            "location": latlng,
+            "latitude": request.form.get("lat"),
+            "longitude": request.form.get("lng"),
             "file": request.form.get("file")
         }
         mongo.db.locations.insert_one(new_location)
 
         flash("Location Added, Thanks for you input")
         return render_template("profile.html")
-    return render_template("add_location.html")
+    return render_template("user_location.html")
 
-    
+
+@app.route("/api/coordinates")
+def coordinates():
+    addresses = mongo.db.locations.find() 
+    print("addresses from the db: ", addresses)
+    all_coords = []  # initialize a list to store addresses
+    for add in addresses:
+        print("add latitude: ", add["latitude"])
+        address_details = {
+            "lat": add["latitude"],
+            "lng": add["longitude"]}
+        all_coords.append(address_details)
+    return jsonify({'coordinates': all_coords})
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
--- static/js/script.js
+++ static/js/script.js
@@ -10,49 +10,6 @@ $(document).ready(function () {
 });
 
 
-var map;
-var marker = false; ////Has the user plotted their location marker? 
-
-//Create map
-function initMap() {
-
-    //The center location of our map.
-    var centerOfMap = new google.maps.LatLng(52.357971, -6.516758);
-
-    //Map options.
-    var options = {
-        center: centerOfMap, //Set center.
-        zoom: 7 //The zoom value.
-    };
-
-    //Create the map object.
-    map = new google.maps.Map(document.getElementById('map'), options);
-
-    //Listen for any clicks on the map.
-    google.maps.event.addListener(map, 'click', function (event) {
-        //Get the location that the user clicked.
-        var clickedLocation = event.latLng;
-        //If the marker hasn't been added.
-        if (marker === false) {
-            //Create the marker.
-            marker = new google.maps.Marker({
-                position: clickedLocation,
-                map: map,
-                draggable: true //make it draggable
-            });
-            //Listen for drag events!
-            google.maps.event.addListener(marker, 'dragend', function (event) {
-                markerLocation();
-            });
-        } else {
-            //Marker has already been added, so just change its location.
-            marker.setPosition(clickedLocation);
-        }
-        //Get the marker's location.
-        markerLocation();
-    });
-}
-
 //This function will get the marker's current location and then add the lat/long
 //values to our textfields so that we can save the location.
 function markerLocation() {
@@ -64,5 +21,70 @@ function markerLocation() {
 }
 
 
-//Load the map when the page has finished loading.
+
+
+
+
+
+
+// Code to sync db output, python and script https://stackoverflow.com/questions/49718569/multiple-markers-in-flask-google-map-api
+var map, i;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: new google.maps.LatLng(53.43237771764574, -6.871380403326448),
+        mapTypeId: 'roadmap'
+    });
+
+    //variable to hold your endpoint
+    var coordAddresses = 'https://8080-amethyst-crow-u769d91j.ws-eu03.gitpod.io/api/coordinates';
+    //an array to hold your coordinates
+    var locations = [];
+    //Using fetch to process the ajax call 
+    // if you use fetch, besure to include the source below this line in your template
+    //<script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.js"></script>
+    fetch(coordAddresses)
+        .then(function (response) {
+            return response.text();
+        }).then(function (body) {
+            var obj = JSON.parse(body);
+            var myAdd = {};
+            var addresses = obj.coordinates;
+            var l = addresses.length;
+        
+            for (i = 0; i < l; i++) {
+                myAdd = {
+                    position: {
+                        lat: parseFloat(obj.coordinates[i].lat),
+                        lng: parseFloat(obj.coordinates[i].lng)
+                    },
+                    title: obj.coordinates[i].title,
+                };
+                locations.push(myAdd);
+            }
+            locations.forEach(function (feature) {
+                var marker = new google.maps.Marker({
+                    position: feature.position,
+                    title: feature.title,
+                    map: map
+                });
+            });
+
+        }).catch(function () {
+            // if the ajax call fails display an error in an info window
+            var pos = {
+                lat: lat,
+                lng: lng
+            };
+            infoWindow.setMap(map);
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('An error occurred, we are unable to retrieve coordinates.');
+
+        });
+
+        // Second map for add_locaton_map window
+        
+}
+
 google.maps.event.addDomListener(window, 'load', initMap);
